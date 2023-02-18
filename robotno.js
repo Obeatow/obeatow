d35704a38e6ac91c4e09fe8d93109272dbd74dfd
Sewell Stephens <sewellstephens104@gmail.com>
2023-02-17 21:05:55 -0600

    hi

---
+++ robotno.js
@@ -0,0 +1,34 @@
+
+if (window.location.href === 'https://tgvyhujik.obeatow.com/') {
+    var norobot = document.createElement('meta');
+
+    document.getElementsByTagName('head')[0].appendChild(norobot);
+    norobot.setAttribute("name", "robots")
+    norobot.setAttribute("content", "noindex")
+
+    setTimeout(() => {
+        window.location.href = 'https://www.obeatow.com';
+    }, 2000);
+}
+else if (window.location.href === 'https://tgvyhujik.obeatow.com/pricing') {
+    var norobot = document.createElement('meta');
+
+    document.getElementsByTagName('head')[0].appendChild(norobot);
+    norobot.setAttribute("name", "robots")
+    norobot.setAttribute("content", "noindex")
+
+    setTimeout(() => {
+        window.location.href = 'https://www.obeatow.com/pricing';
+    }, 2000);
+}
+if (window.location.href === 'https://tgvyhujik.obeatow.com/integrations') {
+    var norobot = document.createElement('meta');
+
+    document.getElementsByTagName('head')[0].appendChild(norobot);
+    norobot.setAttribute("name", "robots")
+    norobot.setAttribute("content", "noindex")
+
+    setTimeout(() => {
+        window.location.href = 'https://www.obeatow.com/integrations';
+    }, 2000);
+}
